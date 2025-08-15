6a99003d07fe1b2d88f8e7b69f39895a02bd7653
JuanCarriles <juanmcarriles@gmail.com>
2025-08-15 13:30:09 -0300

    HU02 Casi completo, faltan permisos de admin

--- src/routes/provinciaRoutes.ts
+++ src/routes/provinciaRoutes.ts
@@ -1,30 +1,34 @@
-import { Router } from 'express';
-import { ProvinciaController } from '../controllers/provinciaController';
-import { provinciaValidation } from '../utils/validations';
-import { handleValidationErrors } from '../middleware/validation';
+import { Router } from "express";
+import { ProvinciaController } from "../controllers/provinciaController";
+import { provinciaValidation } from "../utils/validationsProvincia";
+import { handleValidationErrors } from "../middleware/validation";
 
 const router = Router();
 
 // Rutas para provincias
-router.get('/', ProvinciaController.getAllProvincias);
-router.get('/:id', 
-  provinciaValidation.getById, 
-  handleValidationErrors, 
+router.get("/", ProvinciaController.getAllProvincias);
+router.get(
+  "/:id",
+  provinciaValidation.getById,
+  handleValidationErrors,
   ProvinciaController.getProvinciaById
 );
-router.post('/', 
-  provinciaValidation.create, 
-  handleValidationErrors, 
+router.post(
+  "/",
+  provinciaValidation.create,
+  handleValidationErrors,
   ProvinciaController.createProvincia
 );
-router.put('/:id', 
-  provinciaValidation.update, 
-  handleValidationErrors, 
+router.put(
+  "/:id",
+  provinciaValidation.update,
+  handleValidationErrors,
   ProvinciaController.updateProvincia
 );
-router.delete('/:id', 
-  provinciaValidation.delete, 
-  handleValidationErrors, 
+router.delete(
+  "/:id",
+  provinciaValidation.delete,
+  handleValidationErrors,
   ProvinciaController.deleteProvincia
 );
 
---
+++ src/utils/validationsMarca.ts
@@ -0,0 +1,57 @@
+import { body, param, query } from "express-validator";
+
+export const marcaValidation = {
+  // Validación para crear marca
+  create: [
+    body("descripcion")
+      .notEmpty()
+      .withMessage("La descripción de la marca es requerida")
+      .isLength({ min: 2, max: 100 })
+      .withMessage("La descripción debe tener entre 2 y 100 caracteres")
+      .trim(),
+
+    body("nombre")
+      .notEmpty()
+      .withMessage("El nombre de la marca es requerida")
+      .isLength({ min: 2, max: 50 })
+      .withMessage("El nombre debe tener entre 2 y 50 caracteres")
+      .trim(),
+  ],
+  // Validación para obtener por ID
+  getById: [
+    param("id")
+      .isInt({ min: 1 })
+      .withMessage("El ID debe ser un número entero positivo"),
+  ],
+  update: [
+    param("id")
+      .isInt({ min: 1 })
+      .withMessage("El ID debe ser un número entero positivo"),
+    body("descripcion")
+      .notEmpty()
+      .withMessage("La descripción de la provincia es requerida")
+      .isLength({ min: 2, max: 100 })
+      .withMessage("La descripción debe tener entre 2 y 100 caracteres")
+      .trim(),
+
+    body("nombre")
+      .notEmpty()
+      .withMessage("El nombre de la marca es requerida")
+      .isLength({ min: 2, max: 50 })
+      .withMessage("El nombre debe tener entre 2 y 50 caracteres")
+      .trim(),
+
+    body("activo")
+      .notEmpty()
+      .withMessage("El valor de 'activo' es requerido")
+      .isBoolean()
+      .withMessage("El valor de 'activo' debe ser verdadero o falso")
+      .toBoolean(),
+  ],
+
+  delete: [
+    param("id")
+      .isInt({ min: 1 })
+      .withMessage("El ID debe ser un número entero positivo"),
+  ],
+};
